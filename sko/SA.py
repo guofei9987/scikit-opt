724b303c41096cd3d526ff22b8e8a3b2e30343ef
guofei9987 <guofei9987@foxmail.com>
2019-09-07 17:29:03 +0800

    udf for SA is available

--- sko/SA.py
+++ sko/SA.py
@@ -4,12 +4,12 @@ import numpy as np
 class SA:
     def __init__(self, func, x0, T=100, T_min=1e-7, L=300, q=0.9):
         self.func = func
-        self.x = np.array(x0)
-        self.T = T  # 初始温度
-        self.T_min = T_min  # 终止温度
-        self.L = L  # 各温度下的迭代次数（链长）
-        self.q = q  # 降温速率
-        self.f_list = []  # 记录历史解
+        self.x = np.array(x0)  # initial solution
+        self.T = T  # initial temperature
+        self.T_min = T_min  # end temperature
+        self.L = L  # num of iteration under every temperature（Long of Chain）
+        self.q = q  # cool down speed
+        self.f_list = []  # history
         self.x_star, self.f_star = None, None
 
     def new_x(self, x):
@@ -49,6 +49,15 @@ class SA_TSP(SA):
     def new_x(self, x):
         x = x.copy()
         n1, n2 = np.random.randint(0, len(x), 2)
-        n1, n2 = min(n1, n2), max(n1, n2)
         x[n1], x[n2] = x[n2], x[n1]
         return x
+
+
+def sa_register_udf(udf_func_dict):
+    class SAUdf(SA):
+        pass
+
+    for udf_name in udf_func_dict:
+        if udf_name == 'new_x':
+            SAUdf.new_x = udf_func_dict[udf_name]
+    return SAUdf
