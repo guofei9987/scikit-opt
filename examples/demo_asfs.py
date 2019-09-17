0fde73b7e5b41d54667593b059420d4d4801ca0d
guofei9987 <guofei9987@foxmail.com>
2019-09-17 19:59:27 +0800

    artificial fish swarm algorithm is ready

---
+++ examples/demo_asfs.py
@@ -0,0 +1,10 @@
+def func(x):
+    x1, x2 = x
+    return 1 / x1 ** 2 + x1 ** 2 + 1 / x2 ** 2 + x2 ** 2
+
+
+from sko.ASFA import ASFA
+
+asfa = ASFA(func)
+best_x, best_y = asfa.fit()
+print(best_x, best_y)
