390679aac58353e315bb2b165e2311ced314d146
郭飞 <me@guofei.site>
2019-12-15 18:32:39 +0800

    docs: better description for GE with constraint

--- examples/demo_de.py
+++ examples/demo_de.py
@@ -1,9 +1,9 @@
 '''
 min f(x1, x2, x3) = x1^2 + x2^2 + x3^2
 s.t.
-    x1x2 >= 1
-    x1x2 <= 5
-    x2+x3 = 1
+    x1*x2 >= 1
+    x1*x2 <= 5
+    x2 + x3 = 1
     0 <= x1, x2, x3 <= 5
 '''
 
@@ -30,3 +30,4 @@ de = DE(func=obj_func, n_dim=3, size_pop=50, max_iter=800, lb=[0, 0, 0], ub=[5, 
 
 best_x, best_y = de.run()
 print('best_x:', best_x, '\n', 'best_y:', best_y)
+
