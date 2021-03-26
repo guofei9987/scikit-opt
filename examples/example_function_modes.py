f8121994f8b864d1b9831527d1fb83c43250da42
郭飞 <me@guofei.site>
2021-03-26 09:44:59 +0800

    update test case for multiprocess

--- examples/example_function_modes.py
+++ examples/example_function_modes.py
@@ -1,85 +1,65 @@
-import numpy as np
-from sko.GA import GA
 import time
 import datetime
-from sko.tools import set_run_mode
+import os
+
+print(__name__,os.getpid())
+
+def io_task(name):
+    ppid = os.getppid()
+    pid = os.getpid()
+    start_time = datetime.datetime.now().strftime('%S.%f')
+    time.sleep(1)
+    end_time = datetime.datetime.now().strftime('%S.%f')
+    print('io task, name={name},__name__={__name__}, pid={pid}, ppid ={ppid}, start_time={start_time}, end_time={end_time}\n'.
+          format(name=name,__name__=__name__, pid=pid, ppid=ppid, start_time=start_time, end_time=end_time))
+    return name
+
+def cpu_task(name):
+    ppid = os.getppid()
+    pid = os.getpid()
+    start_time = datetime.datetime.now().strftime('%S.%f')
+    g_search_list = list(range(10000))
+    count = 0
+    for i in range(10000):
+        count += pow(3 * 2, 3 * 2) if i in g_search_list else 0
+
+    end_time = datetime.datetime.now().strftime('%S.%f')
+    print('cpu task, name={name}, pid={pid}, ppid ={ppid}, start_time={start_time}, end_time={end_time}\n'.
+          format(name=name, pid=pid, ppid=ppid, start_time=start_time, end_time=end_time))
+    return name
 
 
-# %% type1: common
-
-def obj_func1(p):
-    time.sleep(0.1)  # say that this function is very complicated and cost 0.1 seconds to run
-    x1, x2 = p
-    x = np.square(x1) + np.square(x2)
-    return 0.5 + (np.square(np.sin(x)) - 0.5) / np.square(1 + 0.001 * x)
 
 
-ga1 = GA(func=obj_func1, n_dim=2, size_pop=10, max_iter=5, lb=[-1, -1], ub=[1, 1], precision=1e-7)
+import datetime
+# from tmp5 import io_task
+# from tmp5 import cpu_task
+from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import Pool
 
-start_time = datetime.datetime.now()
-best_x, best_y = ga1.run()
-print('common mode, time costs: ', (datetime.datetime.now() - start_time).total_seconds())
+# %%
+# if __name__ == '__main__':
+start = datetime.datetime.now()
+pool = ThreadPool()  # ThreadPool(4), 不指定进程数，则使用全部线程
+pool.map(io_task, range(10))  # 返回list，就是结果
+print(datetime.datetime.now() - start)
 
+start = datetime.datetime.now()
+pool = Pool()
+pool.map(io_task, range(10))  # 返回list，就是结果
+print(datetime.datetime.now() - start)
 
-# %% type2:矢量化
+start = datetime.datetime.now()
+list(map(cpu_task, range(10)))
+print('普通任务', datetime.datetime.now() - start)
 
-def obj_func2(p):
-    time.sleep(0.1)  # say that this function is very complicated and cost 1 seconds to run
-    x1, x2 = p[:, 0], p[:, 1]
-    x = np.square(x1) + np.square(x2)
-    return 0.5 + (np.square(np.sin(x)) - 0.5) / np.square(1 + 0.001 * x)
+start = datetime.datetime.now()
+pool = ThreadPool()  # ThreadPool(4), 不指定进程数，则使用全部线程
+pool.map(cpu_task, range(10))  # 返回list，就是结果
+print(datetime.datetime.now() - start)
 
+start = datetime.datetime.now()
+pool = Pool()
+pool.map(cpu_task, range(10))  # 返回list，就是结果
+print(datetime.datetime.now() - start)
 
-set_run_mode(obj_func2, 'vectorization')
-
-ga2 = GA(func=obj_func2, n_dim=2, size_pop=10, max_iter=5, lb=[-1, -1], ub=[1, 1], precision=1e-7)
-start_time = datetime.datetime.now()
-best_x, best_y = ga2.run()
-print('vector mode, time costs: ', (datetime.datetime.now() - start_time).total_seconds())
-
-
-# %% type3：并行化
-
-
-def obj_func3(p):
-    time.sleep(0.1)  # say that this function is very complicated and cost 0.1 seconds to run
-    x1, x2 = p
-    x = np.square(x1) + np.square(x2)
-    return 0.5 + (np.square(np.sin(x)) - 0.5) / np.square(1 + 0.001 * x)
-
-
-set_run_mode(obj_func3, 'parallel')
-ga3 = GA(func=obj_func3, n_dim=2, size_pop=6, max_iter=5, lb=[-1, -1], ub=[1, 1], precision=1e-7)
-start_time = datetime.datetime.now()
-best_x, best_y = ga3.run()
-print('parallel mode, time costs: ', (datetime.datetime.now() - start_time).total_seconds())
-
-
-# %%cache mode
-
-
-def obj_func4_1(p):
-    time.sleep(0.1)  # say that this function is very complicated and cost 0.1 seconds to run
-    x1, x2 = p
-    x = np.square(x1) + np.square(x2)
-    return 0.5 + (np.square(np.sin(x)) - 0.5) / np.square(1 + 0.001 * x)
-
-
-def obj_func4_2(p):
-    time.sleep(0.1)  # say that this function is very complicated and cost 0.1 seconds to run
-    x1, x2 = p
-    x = np.square(x1) + np.square(x2)
-    return 0.5 + (np.square(np.sin(x)) - 0.5) / np.square(1 + 0.001 * x)
-
-
-set_run_mode(obj_func4_2, 'cached')
-ga4_1 = GA(func=obj_func4_1, n_dim=2, size_pop=6, max_iter=10, lb=[-2, -2], ub=[2, 2], precision=1)
-ga4_2 = GA(func=obj_func4_2, n_dim=2, size_pop=6, max_iter=10, lb=[-2, -2], ub=[2, 2], precision=1)
-
-start_time = datetime.datetime.now()
-best_x, best_y = ga4_1.run()
-print('common mode, time costs: ', (datetime.datetime.now() - start_time).total_seconds())
-
-start_time = datetime.datetime.now()
-best_x, best_y = ga4_2.run()
-print('cache mode, time costs: ', (datetime.datetime.now() - start_time).total_seconds())
