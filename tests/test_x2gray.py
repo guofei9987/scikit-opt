e2b163ef9e116c2353d2df3db026eaf035382f62
Ankit Grover <agrover112@gmail.com>
2021-10-15 23:50:59 +0530

    refactor: Refactored for unittest

--- tests/test_x2gray.py
+++ tests/test_x2gray.py
@@ -2,18 +2,26 @@ import numpy as np
 from sko.GA import GA
 from sko.tool_kit import x2gray
 
-cases = [
-    [2, [-1, -1], [1, 1], 1e-7],
-    [5, [-10, -1, -3, -4.5, 1.5], [1, 3, 5, 7.8, 9.8], 1e-7],
-    [3, [0, -5, -10], [15, 10, 5], 1],
+import unittest
 
-]
 
-for n_dim, lb, ub, precision in cases:
-    ga = GA(func=lambda x: x, n_dim=n_dim, size_pop=200, max_iter=800, lb=lb, ub=ub, precision=precision)
+class TestX2Gray(unittest.TestCase):
 
-    value = ga.chrom2x(ga.Chrom)
-    chrom2 = x2gray(x=value, n_dim=n_dim, lb=lb, ub=ub, precision=precision)
+    def test_x2gray(self):
+        cases = [
+            [2, [-1, -1], [1, 1], 1e-7],
+            [5, [-10, -1, -3, -4.5, 1.5], [1, 3, 5, 7.8, 9.8], 1e-7],
+            [3, [0, -5, -10], [15, 10, 5], 1],
 
-    assert (ga.Chrom == chrom2).all(), 'x2gray error'
+        ]
+        for n_dim, lb, ub, precision in cases:
+            ga = GA(func=lambda x: x, n_dim=n_dim, size_pop=200, max_iter=800, lb=lb, ub=ub, precision=precision)
 
+            value = ga.chrom2x(ga.Chrom)
+            chrom2 = x2gray(x=value, n_dim=n_dim, lb=lb, ub=ub, precision=precision)
+            self.assertTrue(np.allclose(ga.Chrom, chrom2),msg='x2gray error')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
