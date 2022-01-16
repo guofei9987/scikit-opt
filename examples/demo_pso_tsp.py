dd420238b863ce8abecf3db0f5815a6602eecb1b
guofei9987 <guofei9987@foxmail.com>
2022-01-16 14:34:19 +0800

    examples: modify comments

--- examples/demo_pso_tsp.py
+++ examples/demo_pso_tsp.py
@@ -2,8 +2,6 @@ import numpy as np
 from scipy import spatial
 import matplotlib.pyplot as plt
 
-# np.random.seed(1)
-
 num_points = 40
 
 points_coordinate = np.random.rand(num_points, 2)  # generate coordinate of points
@@ -18,16 +16,16 @@ def cal_total_distance(routine):
     return sum([distance_matrix[routine[i % num_points], routine[(i + 1) % num_points]] for i in range(num_points)])
 
 
-# %% do GA
+# %% do PSO
 
 from sko.PSO import PSO_TSP
 
 pso_tsp = PSO_TSP(func=cal_total_distance, n_dim=num_points, size_pop=200, max_iter=800, w=0.8, c1=0.1, c2=0.1)
 
-#%%
+# %%
 best_points, best_distance = pso_tsp.run()
 
-print('best_distance',best_distance)
+print('best_distance', best_distance)
 
 # %% plot
 fig, ax = plt.subplots(1, 2)
@@ -36,5 +34,3 @@ best_points_coordinate = points_coordinate[best_points_, :]
 ax[0].plot(best_points_coordinate[:, 0], best_points_coordinate[:, 1], 'o-r')
 ax[1].plot(pso_tsp.gbest_y_hist)
 plt.show()
-
-
